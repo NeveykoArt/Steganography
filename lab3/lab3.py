1aa54faee75599313118d79f4faa421b4b4a8fb0
Neveyko Artem <78909198+NeveykoArt@users.noreply.github.com>
2025-04-26 12:31:47 +0700

    add functionality to set % of embedding

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -21,6 +21,9 @@ class SteganographyApp(QWidget):
         self.encode_label = QLabel("Шифрование")
         layout.addWidget(self.encode_label)
 
+        with open("pg2600.txt", 'r', encoding='utf-8') as f:
+            self.book = f.read()
+
         self.carrier_paths = []
         self.carrier_path_input = QLineEdit()
         self.carrier_path_input.setPlaceholderText("Путь к изображениям-носителям")
@@ -30,9 +33,9 @@ class SteganographyApp(QWidget):
         self.carrier_button.clicked.connect(self.select_carrier_images)
         layout.addWidget(self.carrier_button)
 
-        self.secret_msg_input = QLineEdit()
-        self.secret_msg_input.setPlaceholderText("Секретное сообщение")
-        layout.addWidget(self.secret_msg_input)
+        self.secret_msg_percentage = QLineEdit()
+        self.secret_msg_percentage.setPlaceholderText("Процент заполнения")
+        layout.addWidget(self.secret_msg_percentage)
 
         self.encode_button = QPushButton("Зашифровать")
         self.encode_button.clicked.connect(self.hide_message)
@@ -76,73 +79,89 @@ class SteganographyApp(QWidget):
             self.stego_paths = file_dialog.selectedFiles()
             self.stego_path_input.setText("; ".join(self.stego_paths))
 
-    def hide_message(self):
-        secret_msg = self.secret_msg_input.text()
+    def get_stego_text_chunk(self, container_bits: int) -> str:
+        self.target_bits = int(container_bits * (int(self.secret_msg_percentage.text()) / 100))
+        target_bytes = self.target_bits // 8  # 1 символ = 1 байт
+        
+        return self.book[:target_bytes]
 
-        if not self.carrier_paths or not secret_msg:
+    def hide_message(self):
+        if not self.carrier_paths:
             QMessageBox.warning(self, "Ошибка", "Пожалуйста, выберите изображения-носители и введите сообщение.")
             return
 
         try:
             for carrier_path in self.carrier_paths:
-                img = Image.open(carrier_path)
-                pixels = np.array(img)
+                max_hide, _ = self.embedding(self.book, carrier_path)
+                secret_msg = self.get_stego_text_chunk(max_hide)
+                _, pixels = self.embedding(secret_msg, carrier_path)
 
-                msg_bits = ''.join(format(ord(c), '08b') for c in secret_msg) + '00000000'
-                msg_pairs = [msg_bits[i:i+2] for i in range(0, len(msg_bits), 2)]
-
-                height, width = pixels.shape
-                pair_idx = 0
-
-                for row in range(height):
-                    for col in range(width):
-                        if pair_idx >= len(msg_pairs):
-                            break
-
-                        pixel = pixels[row, col]
-                        msb4 = (pixel & 0xF0) >> 4  # Получение 4 старших битов
-
-                        # Формирование пар битов из MSB
-                        leftpare = (msb4 & 0b1100) >> 2  # Биты 7-6
-                        middlepare = (msb4 & 0b0110) >> 1  # Биты 6-5
-                        rightpare = msb4 & 0b0011  # Биты 5-4
-
-                        # Сравнение пар и модификация LSB
-                        lsb3 = pixel & 0b111  # Младшие 3 бита
-
-                        if pair_idx < len(msg_pairs) and leftpare == int(msg_pairs[pair_idx], 2):
-                            lsb3 |= (1 << 2)  # Установка 3-го LSB
-                            pair_idx += 1
-                        else:
-                            lsb3 &= ~(1 << 2) & 0b111  # Сброс 3-го LSB
-
-                        if pair_idx < len(msg_pairs) and middlepare == int(msg_pairs[pair_idx], 2):
-                            lsb3 |= (1 << 1)  # Установка 2-го LSB
-                            pair_idx += 1
-                        else:
-                            lsb3 &= ~(1 << 1) & 0b111  # Сброс 2-го LSB
-
-                        if pair_idx < len(msg_pairs) and rightpare == int(msg_pairs[pair_idx], 2):
-                            lsb3 |= (1 << 0)  # Установка 1-го LSB
-                            pair_idx += 1
-                        else:
-                            lsb3 &= ~(1 << 0) & 0b111  # Сброс 1-го LSB
-
-                        # Обновление значения пикселя
-                        new_pixel = (msb4 << 4) | lsb3
-                        pixels[row, col] = new_pixel
-
-                # Сохранение стегоизображения
                 filename, file_extension = os.path.splitext(carrier_path)
-                output_path = f"{filename}_stego_lab3{file_extension}"
+                output_path = f"{filename}_stego_lab3_{self.secret_msg_percentage.text()}{file_extension}"
                 self.stego_paths.append(output_path)
                 Image.fromarray(pixels.astype(np.uint8)).save(output_path)
                 self.decoded_msg_output.append(f"Сообщение зашифровано в {output_path}\n")
+                self.decoded_msg_output.append(f"встроено {self.target_bits} бит\n")
+
 
             QMessageBox.information(self, "Успех", "Сообщение зашифровано во все выбранные изображения.")
 
         except Exception as e:
             QMessageBox.critical(self, "Ошибка", f"Ошибка: {e}")
+
+    def embedding(self, secret_msg, carrier_path):
+        img = Image.open(carrier_path)
+        pixels = np.array(img)
+
+        msg_bits = ''.join(format(ord(c), '08b') for c in secret_msg) + '00000000'
+        msg_pairs = [msg_bits[i:i+2] for i in range(0, len(msg_bits), 2)]
+
+        height, width = pixels.shape
+        pair_idx = 0
+
+        max_hide = 0
+
+        for row in range(height):
+            for col in range(width):
+                if pair_idx >= len(msg_pairs):
+                    break
+
+                pixel = pixels[row, col]
+                msb4 = (pixel & 0xF0) >> 4  # Получение 4 старших битов
+
+                # Формирование пар битов из MSB
+                leftpare = (msb4 & 0b1100) >> 2  # Биты 7-6
+                middlepare = (msb4 & 0b0110) >> 1  # Биты 6-5
+                rightpare = msb4 & 0b0011  # Биты 5-4
+
+                # Сравнение пар и модификация LSB
+                lsb3 = pixel & 0b111  # Младшие 3 бита
+
+                if pair_idx < len(msg_pairs) and leftpare == int(msg_pairs[pair_idx], 2):
+                    lsb3 |= (1 << 2)  # Установка 3-го LSB
+                    pair_idx += 1
+                    max_hide += 2
+                else:
+                    lsb3 &= ~(1 << 2) & 0b111  # Сброс 3-го LSB
+
+                if pair_idx < len(msg_pairs) and middlepare == int(msg_pairs[pair_idx], 2):
+                    lsb3 |= (1 << 1)  # Установка 2-го LSB
+                    pair_idx += 1
+                    max_hide += 2
+                else:
+                    lsb3 &= ~(1 << 1) & 0b111  # Сброс 2-го LSB
+
+                if pair_idx < len(msg_pairs) and rightpare == int(msg_pairs[pair_idx], 2):
+                    lsb3 |= (1 << 0)  # Установка 1-го LSB
+                    pair_idx += 1
+                    max_hide += 2
+                else:
+                    lsb3 &= ~(1 << 0) & 0b111  # Сброс 1-го LSB
+
+                # Обновление значения пикселя
+                new_pixel = (msb4 << 4) | lsb3
+                pixels[row, col] = new_pixel
+        return max_hide, pixels
 
     def extract_message(self):
         if not self.stego_paths:
